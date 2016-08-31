952f32bd5776b9f5f9c303ea96ffa1c15a7d3311
David Baker <dbaker@acorncomputersolutions.com>
2016-08-30 20:57:51 -0600

    Tweaked regex to match {{rootUrl}} strings in our asset lines

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ module.exports = {
         const replaceOptions = {
           files: ['index.html'],
           patterns: [{
-            match: /<.*?script src=\"assets\/.*.js\".*?>.*?<\/.*?script.*?>/g,
+            match: /<.*?script src=\".*?assets\/.*.js\"?>.*?<\/.*?script.*?>/g,
             replacement: ''
           }]
         };
